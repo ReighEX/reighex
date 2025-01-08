aee13c61d9359a5fa828dbc537883ae23b5ade50
ReighEX <alyanrizky@gmail.com>
2025-01-08 23:56:07 +0700

    bagian porto blender modeling 1

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("myModal");
+    var img = document.getElementById("myPhoto");
+    var modalImg = document.getElementById("img01");
+    var span = document.getElementsByClassName("close")[0];
+
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+});
+
