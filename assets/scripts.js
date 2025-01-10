cf51f246a3c653ca380e60a49c7088da537b2550
ReighEX <alyanrizky@gmail.com>
2025-01-11 00:56:58 +0700

    Revisi dikit terutama linkedin taro di contact me dsbnya

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,12 +2,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('button');
 
-    buttons.forEach(button => {
+    /*buttons.forEach(button => {
         button.addEventListener('click', function() {
             const link = this.getAttribute('onclick').split("'")[1];
             window.location.href = link;
         });
-    });
+    }); */
 });
 
 document.addEventListener("DOMContentLoaded", function() {
